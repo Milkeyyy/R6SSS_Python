ee60a9dd0aa809b32ff7a3e25d5b731d0b657d23
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2024-10-17 02:56:28 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup, find_packages
 
-def get_requirements_from_file():
-	with open("./requirements.txt") as f:
-		r = f.read().splitlines()
-	return r
 
 setup(
 	name="r6sss",
@@ -11,5 +7,4 @@ setup(
 	description="Rainbow Six Siege Server Status Web API Wrapper Library",
 	author="Milkeyyy",
 	packages=find_packages(),
-	requires=get_requirements_from_file()
-)
+	requires=["httpx"]
